60114ae4593bbdd10fa8c4ca3b275b3e750441a8
Beginner-Project <boripats63@nu.ac.th>
2024-07-18 22:53:59 +0700

    chore: rename function.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import Home from "./home";
 
-export default function Home() {
+export default function App() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between w-full`}
